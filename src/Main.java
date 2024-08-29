aa7df5941fc610974b91490130c07c46d425c3ad
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-29 13:02:43 -0400

    cat

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    private static boolean isCatPlaying(boolean summer, int temperature) {
+        if(summer){
+            if (temperature >= 25 && temperature<= 45){
+                return true;
+            }
+        }else{
+            if (temperature >= 25 && temperature <= 35){
+                return true;
+            }
+        }
+        return false;
+
+
+    }
 }
+
+
+
+
+
+
